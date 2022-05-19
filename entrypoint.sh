e235aeb888723a4fd2c1c482dac5aadccdd6d018
NFS86 <jarbull86@gmail.com>
2022-05-19 11:23:27 +0000

    Test again

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,7 @@
 
 set -eu
 
-bash -c  "nohup tail -f /root/.megaCmd/megacmdserver.log >&0 2>1" &
-if [[ "$USERNAME" != "NOBODY" ]] && [[ "$PASSWORD" != "CHANGEME" ]]; then
-    mega-login $USERNAME $PASSWORD
-fi
-
-if [[ "$*" == "" ]]; then
-	echo "Please specify a valid megacli command"
-    	exit 1
-fi
+bash -c  "nohup tail -f /root/.megaCmd/megacmdserver.log >&0 2>1"
 
 if [[ -n "$RCLONE_CONF" ]]
 then
